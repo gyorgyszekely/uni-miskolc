0db9463413d3e062ecf035585c8b212965bab670
spozsgai <sanyika-3232>
2016-05-07 12:49:55 +0200

    Hiányzó login activity, hibák javítása.

--- client/app/src/main/java/hu/miskolc/uni/iit/hydrominder/MainActivity.java
+++ client/app/src/main/java/hu/miskolc/uni/iit/hydrominder/MainActivity.java
@@ -62,12 +62,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Új gombra kattintva új időpontot lehet felvenni
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                new NewReminderDialog(MainActivity.this).Open();
             }
         });
 
